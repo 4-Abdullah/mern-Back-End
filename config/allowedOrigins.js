27c936025d65724200def9724d4be7e15c29a0d9
4-Abdullah <abdullahmaliksher@gmail.com>
2025-11-12 13:22:31 +0500

    allowed Origin updated

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'https://mern-admin-dashboard-phi.vercel.app',
     'https://front-end-lemon-seven.vercel.app',
-    
+    'http://localhost:3000',
     'http://localhost:3001',
 ];
 
